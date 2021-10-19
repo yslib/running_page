792b712a63c32b65b84487c0ab0fc01150141197
ysl <visysl@outlook.com>
2021-10-19 22:33:56 +0800

    fixup

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,17 +2,17 @@ module.exports = {
   pathPrefix: '/running_page', // Change to `/running_page` when running on github pages
   siteMetadata: {
     siteTitle: 'Running Page',
-    siteUrl: 'https://yihong.run',
+    siteUrl: 'https://yslphotos.com',
     logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
     description: 'Personal site and blog',
     navLinks: [
       {
         name: 'Blog',
-        url: 'https://yihong.run/running',
+        url: 'https://visysl.com',
       },
       {
-        name: 'About',
-        url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
+        name: 'Yields the Story of Life',
+        url: 'https://picysl.com',
       },
     ],
   },
