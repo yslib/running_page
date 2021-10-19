fa6151cfab134bcbeac0354d72824ae6e54cea1c
ysl <visysl@outlook.com>
2021-10-19 22:28:06 +0800

    fixup

--- src/utils/const.js
+++ src/utils/const.js
@@ -1,6 +1,6 @@
 // const
 const MAPBOX_TOKEN =
-  'pk.eyJ1IjoieWlob25nMDYxOCIsImEiOiJja2J3M28xbG4wYzl0MzJxZm0ya2Fua2p2In0.PNKfkeQwYuyGOTT_x9BJ4Q';
+  'pk.eyJ1IjoibHVjaWRhIiwiYSI6ImNrdXk1dzB5ZDJkYjkyb2xuMHBqdnRrNmYifQ.AgOnEsp38ZXvl5NneENEww';
 const MUNICIPALITY_CITIES_ARR = [
   '北京市',
   '上海市',
